85ea407720391b4ab4a3e8a0a3e7b519e755a8ee
Lais Frigério <laaisfrigerio@gmail.com>
2019-02-05 22:40:26 -0200

    Remove show dev tool
    Signed-off-by: Lais Frigério <laaisfrigerio@gmail.com>

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const createWindow = () => {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 600,
+        height: 700,
     });
 
     // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', () => {
